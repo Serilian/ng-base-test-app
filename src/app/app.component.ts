3fa245765fb119533b6609f5e88a00c308e8cbe1
Filip <fhagno@yahoo.com>
2018-06-17 11:40:17 +0200

    Adding variable for jumbotron element.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  jbtHeading = 'Hello World';
+  jbtButton = 'Learn More';
+  jbtText = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique';
+  jbtBtnUrl = 'posts';
 
 }
